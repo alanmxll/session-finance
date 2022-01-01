26927d37f0a942bc521a3691e578b3c393dfeb54
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-01-01 20:22:31 -0300

    chore: implements health check endpoint

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,12 +3,15 @@ package http
 import (
 	"net/http"
 
+	"github.com/alanmxll/session-finance/adapter/http/actuator"
 	"github.com/alanmxll/session-finance/adapter/http/transaction"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
+
+	http.HandleFunc("/health", actuator.Health)
 
 	http.ListenAndServe(":8080", nil)
 }
